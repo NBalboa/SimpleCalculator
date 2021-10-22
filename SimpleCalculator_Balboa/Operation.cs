91ce098c65be8b8b4a4696cede30a5d8f0c6cd66
Nbalboa <nickojek2x@gmail.com>
2021-10-22 17:47:34 +0800

    Simple Calculator

---
+++ SimpleCalculator_Balboa/Operation.cs
@@ -0,0 +1,26 @@
+﻿namespace SimpleCalculator_Balboa
+{
+    public class Operation
+    {
+        public int Addition(int firstNumber, int secondNumber)
+        {
+            int output = firstNumber + secondNumber;
+            return output;
+        }
+        public int Subtraction(int numberOne, int numberTwo)
+        {
+            int output = numberOne - numberTwo;
+            return output;
+        }
+        public int Multiplication(int numberOne, int numberTwo)
+        {
+            int output = numberOne * numberTwo;
+            return output;
+        }
+        public int Division(int numberOne, int numberTwo)
+        {
+            int output = numberOne / numberTwo;
+            return output;
+        }
+    }
+}
---
+++ SimpleCalculator_Balboa/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator_Balboa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+            string output = "";
+            string tryAgain = "";
+            bool isContinue = true;
+
+            do
+            {
+                bool flag = true;
+                Console.WriteLine("Welcome to Simple Calculator\n");
+
+                firstNumber = GetInt("Enter First Number: ");
+                secondNumber = GetInt("Enter Second Number: ");
+
+                output = GetInfoFromConsole(firstNumber, secondNumber);
+                Console.WriteLine(output);
+
+
+                tryAgain = GetString("Do you want to try again(Y/N): ");
+
+                while (flag)
+                {
+                    if (tryAgain.ToLower() == "y")
+                    {
+                        flag = false;
+                        Console.Clear();
+                    }
+                    else if (tryAgain.ToLower() == "n")
+                    {
+                        flag = false;
+                        isContinue = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input");
+                        tryAgain = GetString("Do you want to try again(Y/N): ");
+                    }
+                }
+
+            } while (isContinue);
+
+
+            Console.WriteLine("End of the Program");
+        }
+        public static int GetInt(string message)
+        {
+            string numberText = GetString(message);
+            int number;
+            bool isValidNumber = int.TryParse(numberText, out number);
+
+            while (!isValidNumber)
+            {
+                Console.WriteLine("Invalid input. Please enter a digit");
+                numberText = GetString(message);
+                isValidNumber = int.TryParse(numberText, out number);
+            }
+
+            return number;
+        }
+        public static string GetString(string message)
+        {
+            string output = "";
+            while (string.IsNullOrEmpty(output))
+            {
+                Console.Write(message);
+                output = Console.ReadLine();
+            }
+
+            return output;
+        }
+        public static void ShowUI()
+        {
+            Console.WriteLine("Simple Calculator:");
+            Console.WriteLine("[1] Addition");
+            Console.WriteLine("[2] Subtraction");
+            Console.WriteLine("[3] Multiplication");
+            Console.WriteLine("[4] Division");
+        }
+
+        public static string GetInfoFromConsole(int firstNumber, int secondNumber)
+        {
+            int result = 0;
+            string output = "";
+            string select = "";
+            bool flag = true;
+           
+            Operation calculate = new Operation();
+
+            
+            ShowUI();
+
+            while (flag)
+            {
+                select = GetString("Select Computation: ");
+
+                if (select == "1")
+                {
+                    result = calculate.Addition(firstNumber, secondNumber);
+                    output = $"{firstNumber} + {secondNumber} = {result}";
+                    flag = false;
+                }
+                else if (select == "2")
+                {
+                    result = calculate.Subtraction(firstNumber, secondNumber);
+                    output = $"{firstNumber} - {secondNumber} = {result}";
+                    flag = false;
+                }
+                else if (select == "3")
+                {
+                    result = calculate.Multiplication(firstNumber, secondNumber);
+                    output = $"{firstNumber} * {secondNumber} = {result}";
+                    flag = false;
+                }
+                else if (select == "4")
+                {
+                    result = calculate.Division(firstNumber, secondNumber);
+                    output = $"{firstNumber} / {secondNumber} = {result}";
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input.");
+                } 
+            }
+
+            return output;
+        }
+    }
+}
